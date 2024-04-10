562dbc85812c36dde1efd12756a53272940fa482
bethanyhipple-dbtlabs <108838013+bethanyhipple-dbtlabs@users.noreply.github.com>
2024-04-10 13:49:14 -0700

    Update dim_customers.sql

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -12,7 +12,7 @@ customer_orders as (
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
         count(order_id) as number_of_orders,
-        sum(amount) as lifetime_value
+        sum(amount_usd) as lifetime_value
     from orders
     group by 1
 ),
