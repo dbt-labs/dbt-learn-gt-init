75ffd9d0cbda835c2daad6aabe19c541a75cb550
David Opoku <david.opoku@dbtlabs.com>
2024-12-14 09:22:03 +0000

    Materialize the fct_page_views model as incremental.

--- models/marts/fct_page_views.sql
+++ models/marts/fct_page_views.sql
@@ -1,16 +1,25 @@
 {{
     config(
-        tags='nightly'
+        materialized='incremental',
+        unique_key= 'page_view_id'
     )
 }}
 
+
 with events as (
     select * from {{ ref('stg_snowplow__events') }}
+    {% if is_incremental() %}
+        -- this filter will only be applied on an incremental run
+        where collector_tstamp > (select dateadd('day', -3, max(max_collector_tstamp)) from {{ this }})
+    {% endif %}
+
 ),
+
 page_views as (
     select * from events
     where event = 'page_view'
 ),
+
 aggregated_page_events as (
     select
         page_view_id,
@@ -20,10 +29,12 @@ aggregated_page_events as (
     from events
     group by 1
 ),
+
 joined as (
     select
         *
     from page_views
     left join aggregated_page_events using (page_view_id)
 )
+
 select * from joined
