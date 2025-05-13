a91fd41491a848f1361fc573ae42b7139d02c8be
Chau Nguyen <chau.nguyen@dbtlabsworkshop.com>
2025-05-13 18:59:52 +0000

    End of session 1

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -1,11 +1,6 @@
 with customers as (
 
-    select
-        id as customer_id,
-        first_name,
-        last_name
-
-    from raw.jaffle_shop.customers
+  select * from {{ ref('stg_jaffle_shop__customers') }}
 
 ),
 
---
+++ models/staging/stg_jaffle_shop__orders.sql
@@ -0,0 +1,8 @@
+select
+    id as order_id,
+    user_id as customer_id,
+    order_date,
+    status,
+    _etl_loaded_at
+
+from raw.jaffle_shop.orders
