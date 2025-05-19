2239e5a519a75d1d1b10f22ec145738a180f8610
Dan Horner <dan.horner@dbtlabsworkshop.com>
2025-05-19 20:35:09 +0000

    end of day 2 of fundamentals training

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -6,14 +6,7 @@ with customers as (
 
 orders as (
 
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status,
-        _etl_loaded_at
-
-    from raw.jaffle_shop.orders
+    select * from {{ ref('stg_jaffle_shop__orders') }}
 
 ),
 
