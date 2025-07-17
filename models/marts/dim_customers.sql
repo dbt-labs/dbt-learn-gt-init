6be169d355d57620d3ee5fbb17248653ab1dba9d
Luan Nguyen <luan.nguyen@dbtlabsworkshop.com>
2025-07-17 17:03:40 +0000

    updated changes

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -1,11 +1,11 @@
 with customers as (
 
     select
-        id as customer_id,
+        customer_id,
         first_name,
         last_name
 
-    from raw.jaffle_shop.customers
+    from {{ref('stg_jaffle_shop__customers')}}
 
 ),
 
@@ -13,12 +13,12 @@ orders as (
 
     select
         id as order_id,
-        user_id as customer_id,
+        customer_id,
         order_date,
         status,
         _etl_loaded_at
 
-    from raw.jaffle_shop.orders
+    from {{ref ('stg_jaffle_shop__orders')}}
 
 ),
 
