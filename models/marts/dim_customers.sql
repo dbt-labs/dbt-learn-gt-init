e61ecb3755cb0b81905f575132b1e4438bc7b532
Stefanos Stoikos <stefanos.stoikos@dbtlabsworkshop.com>
2025-09-11 20:12:35 +0000

    commit

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -1,44 +1,22 @@
 with customers as (
-
-    select
-        id as customer_id,
-        first_name,
-        last_name
-
-    from raw.jaffle_shop.customers
-
+    select * from {{ ref('stg_jaffle_shop__customers') }}
 ),
 
 orders as (
-
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
-
-    from raw.jaffle_shop.orders
-
+    select * from {{ ref('stg_jaffle_shop__orders') }}
 ),
 
 customer_orders as (
-
     select
         customer_id,
-
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
         count(order_id) as number_of_orders
-
     from orders
-
-    group by 1
-
+    group by customer_id
 ),
 
-
 final as (
-
     select
         customers.customer_id,
         customers.first_name,
@@ -46,11 +24,8 @@ final as (
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
         coalesce(customer_orders.number_of_orders, 0) as number_of_orders
-
     from customers
-
     left join customer_orders using (customer_id)
-
 )
 
 select * from final
