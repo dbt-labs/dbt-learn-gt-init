3047cd09296c3e30a9f53d10f17fe3bb48d5fc73
Alex Haynes <alex.haynes@dbtlabsworkshop.com>
2025-09-11 19:17:25 +0000

    Working state

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -1,7 +1,7 @@
 select
-    idas order_id,
+    id as order_id,
     user_id as customer_id,
     order_date,
     status,
     _etl_loaded_at
-from raw.jaffle_shop.orders
+from {{ source('jaffle_shop', 'orders') }}
