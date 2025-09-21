c4f63c8fc38008c6ad1ea906b190877b31f941fb
rag <ricardo020796@gmail.com>
2025-09-21 10:59:47 -0400

    Nuevos documents staging

--- models/customers.sql
+++ models/customers.sql
@@ -1,23 +1,14 @@
+{{ config(materialized='view') }}
+
 with customers as (
 
-    select
-        id as customer_id,
-        first_name,
-        last_name
-
-    from raw.jaffle_shop.customers
+select * from {{ ref('stg_jaffle_shop_customers') }}
 
 ),
 
 orders as (
 
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
-
-    from raw.jaffle_shop.orders
+select * from {{ ref('stg_jaffle_shop_orders') }}
 
 ),
 
