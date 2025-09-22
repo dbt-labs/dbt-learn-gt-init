9e0ec382ac7a5f8d15070124386621c62f76b8b2
Alejandro Hidalgo <alejandro.hidalgo@dbtlabsworkshop.com>
2025-09-22 21:02:29 +0000

    End of class 1

---
+++ models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
@@ -0,0 +1,22 @@
+with 
+
+source as (
+
+    select * from {{ source('jaffle_shop', 'orders') }}
+
+),
+
+renamed as (
+
+    select
+        id as order_id,
+        user_id as customer_id,
+        order_date,
+        status as order_status,
+        _etl_loaded_at
+
+    from source
+
+)
+
+select * from renamed
