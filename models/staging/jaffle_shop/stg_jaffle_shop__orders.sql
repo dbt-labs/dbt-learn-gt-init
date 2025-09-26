8b2ee00ca7bb5d32a1ea10122c9afa9ff8bb04a0
Jordan Kinney <jordan.kinney@dbtlabsworkshop.com>
2025-09-26 15:35:40 +0000

    added staging models, tests, documentation to our jaffle shop project

---
+++ models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
@@ -0,0 +1,21 @@
+with 
+
+source as (
+
+    select * from {{ source('jaffle_shop', 'orders') }}
+
+),
+
+renamed as (
+
+    select
+        id as order_id,
+        user_id as customer_id,
+        order_date,
+        status,
+        _etl_loaded_at
+
+    from source
+
+)
+Select * from renamed
