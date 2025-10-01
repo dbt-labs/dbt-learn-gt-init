7f14fe25d6c20ae7b3980496e0a2fff5ec935c0b
sanatan desai <sanatan.desai+1759345538@dbtlabsworkshop.com>
2025-10-01 21:00:19 +0000

    added stg_jaffle_shop__customers.sql

--- models/marts/customers.sql
+++ models/marts/customers.sql
@@ -1,11 +1,18 @@
+{{
+    config(
+        materialized='table'
+    )
+}}
+
 with customers as (
 
     select
-        id as customer_id,
+        customer_id,
         first_name,
         last_name
 
-    from raw.jaffle_shop.customers
+    from {{ ref('stg_jaffle_shop__customers') }}
+    
 
 ),
 
---
+++ models/staging/jaffle_shop/stg_jaffle_shop__customers.sql
@@ -0,0 +1,7 @@
+select
+        id as customer_id,
+        first_name,
+        last_name
+
+    from raw.jaffle_shop.customers
+    
