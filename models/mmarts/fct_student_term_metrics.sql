8156236abcda5c72825a73d1e4ae898ab8254bde
bethanyhipple-dbtlabs <108838013+bethanyhipple-dbtlabs@users.noreply.github.com>
2025-10-03 10:14:41 -0700

    Update and rename fct_student_term_metrics.sql to models/mmarts/fct_student_term_metrics.sql

--- models/mmarts/fct_student_term_metrics.sql
+++ models/mmarts/fct_student_term_metrics.sql
@@ -1,8 +1,12 @@
 -- fact: student-term level metrics using intermediate aggregates
+with metrics as (
+    select * from {{ ref('int_student_term_aggregates') }}
+    )
+
 select
     student_id,
     semester_id,
     courses_taken,
     credits_taken,
     gpa_estimate
-from {{ ref('int_student_term_aggregates') }}
+from metrics
