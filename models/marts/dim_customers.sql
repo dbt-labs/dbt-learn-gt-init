860e50cad5adf53ec80c2843a55de88be6f67df1
jong belangdal <jong.belangdal+1759852914@dbtlabsworkshop.com>
2025-10-07 21:47:51 +0000

    commit Session 3

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -7,7 +7,7 @@
 with customers as (
 
     select
-        id as customer_id,
+        customer_id,
         first_name,
         last_name
 
@@ -17,12 +17,12 @@ with customers as (
 orders as (
 
     select
-        id as order_id,
-        user_id as customer_id,
+        order_id,
+        customer_id,
         order_date,
         status
 
-    from raw.jaffle_shop.orders
+    from {{ ref('stg_jaffle_shop__orders') }}
 
 ),
 
--- models/staging/jaffle_shop/stg_jaffle_shop__customers.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__customers.sql
@@ -1,6 +1,7 @@
-    select
-        id as customer_id,
-        first_name,
-        last_name
+select
+    id as customer_id,
+    first_name,
+    last_name
 
-    from raw.jaffle_shop.customers
+--from raw.jaffle_shop.customers
+from {{ source('jaffle_shop', 'customers') }}
