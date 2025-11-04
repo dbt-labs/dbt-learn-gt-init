24cb4d79cbcd81232922426d326c889d5123e43e
cam kolk <cam.kolk+1762272417@dbtlabsworkshop.com>
2025-11-04 22:10:46 +0000

    practice declaring sources

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -5,7 +5,7 @@ with customers as (
         first_name,
         last_name
 
-    from raw.jaffle_shop.customers
+    from {{ ref('stg_jaffle_shop__customers') }}
 
 ),
 
@@ -17,7 +17,7 @@ orders as (
         order_date,
         status
 
-    from raw.jaffle_shop.orders
+    from {{ ref('stg_jaffle_shop__orders') }}
 
 ),
 
