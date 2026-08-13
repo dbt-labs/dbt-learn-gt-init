842137fcb6e3848d97bd1ef7b8049dac12c7e5d8
Sebastian Davis <davis.g.sebastian@gmail.com>
2026-08-12 22:10:49 -0700

    added 2 staging models, refactored dim_customers, establisehd materialization strat

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -1,23 +1,12 @@
 with customers as (
 
-    select
-        id as customer_id,
-        first_name,
-        last_name
-
-    from dbt-tutorial.jaffle_shop.customers
+    select * from {{ ref('stg_jaffle_shop__customers') }}
 
 ),
 
 orders as (
 
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
-
-    from dbt-tutorial.jaffle_shop.orders
+    select * from {{ ref('stg_jaffle_shop__orders')}}
 
 ),
 
