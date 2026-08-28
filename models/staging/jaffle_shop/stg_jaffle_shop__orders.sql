d6cd3977ee2408a3cdbb392468fa46e001e5b0eb
Adam <aabramsnt@gmail.com>
2026-08-28 14:09:18 -0400

    added source and freshness macros

--- models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
@@ -2,6 +2,7 @@
         id as order_id,
         user_id as customer_id,
         order_date,
-        status as order_status
-
-    from raw.jaffle_shop.orders
+        status as order_status,
+        _etl_loaded_at
+        
+    from {{ source('jaffle_shop', 'orders') }}
