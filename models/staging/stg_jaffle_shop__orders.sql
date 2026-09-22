ec6aab6af1219304559ea83dfdf4d925f22f4903
elliot leibowitz <elliot.leibowitz+1790090669@dbtlabsworkshop.com>
2026-09-22 16:59:56 +0000

    configured jaffle_shop sources, created customers and orders staging models and implemented source/ref macros

--- models/staging/stg_jaffle_shop__orders.sql
+++ models/staging/stg_jaffle_shop__orders.sql
@@ -1,10 +1,3 @@
---stg_jaffle_shop__orders.sql
-
-select
-    id as order_id,
-    user_id as customer_id,
-    order_date,
-    status,
-        _etl_loaded_at
-
-from raw.jaffle_shop.orders
+-- stg_jaffle_shop__orders.sql
+select id as order_id, user_id as customer_id, order_date, status, _etl_loaded_at
+from {{ source('jaffle_shop', 'orders') }}
