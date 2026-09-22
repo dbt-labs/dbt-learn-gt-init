cf8ca655248f5bcdb9a8f6b6417ce2dd590f6e48
amandeep kohli <amandeep.kohli+1790090688@dbtlabsworkshop.com>
2026-09-22 17:04:41 +0000

    commit

---
+++ models/staging/stg_jaffle_shop__customers.sql
@@ -0,0 +1,9 @@
+with customers as (
+        select
+        id as customer_id,
+        first_name,
+        last_name
+    from {{ source('jaffle_shop', 'customers') }}
+
+)
+select * from customers
--- models/staging/stg_jaffle_shop__orders.sql
+++ models/staging/stg_jaffle_shop__orders.sql
@@ -5,6 +5,6 @@ with orders as (
         order_date,
         status,
         _etl_loaded_at
-    from raw.jaffle_shop.orders
+    from {{ source('jaffle_shop', 'orders') }}
 )
 select * from orders
