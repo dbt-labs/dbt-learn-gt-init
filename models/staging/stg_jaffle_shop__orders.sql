5132a4de54c87137290ac6d0696c089d5f45af8c
sam magdum <sam.magdum@dbtlabsworkshop.com>
2026-09-23 12:59:11 +0000

    configured my jaffle shop sources, created customers and orders staging models and implemented src/ ref macros

--- models/staging/stg_jaffle_shop__orders.sql
+++ models/staging/stg_jaffle_shop__orders.sql
@@ -1,10 +1,7 @@
---stg_jaffle_shop__orders.sql
-
 select
-    id as order_id,
-    user_id as customer_id,
-    order_date,
-    status,
-  _etl_loaded_at
-
-from raw.jaffle_shop.orders
+    id as order_id,
+    user_id as customer_id,
+    order_date,
+    status,
+    _etl_loaded_at
+from {{ source('jaffle_shop', 'orders') }}
